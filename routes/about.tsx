2055a3d65746d1f09fcb3eded0da7124140926f7
ix0rai <ix0rai64@gmail.com>
2022-10-24 21:35:38 -0500

    style a little

--- routes/about.tsx
+++ routes/about.tsx
@@ -1,30 +1,26 @@
+import { tw } from "twind";
+
 export default function AboutPage() {
     return (
         <html>
-            <head>
-                <style>
-                    div.container {}
-                </style>
-            </head>
             <div class="p-4 mx-auto max-w-screen-md" style="text-align:center">
                 <h1>About</h1>
                 <h2>This is the about page.</h2>
                 <br></br>
                 <p>my links:</p>
-                <div class="container">
-                    <ul>
-                        <li><a href="https://www.curseforge.com/members/ix0rai">curseforge</a></li>
-                        <li><a href="https://github.com/ix0rai">github</a></li>
-                        <li><a href="https://steamcommunity.com/id/ix0rai/">steam</a></li>
-                        <li><a href="https://www.twitch.tv/ix0rai/">twitch</a></li>
-                        <li><a href="https://youtube.com/@ix0rai">youtube</a></li>
-                        <li><a href="mailto:ix0rai64@gmail.com">email</a></li>
-                        <li><a href="https://en.pronouns.page/@ix0rai">pronouns.page</a></li>
-                        <li><a href="https://modrinth.com/user/ix0rai/">modrinth</a></li>
-                        <li><a href="https://twitter.com/ix0rai">twitter</a></li>
-                    </ul>
-                    
-                </div>
+                <ul class={
+                    tw({ 'bg-pink-200': true, rounded: true, underline: true,})
+                }>
+                    <li><a href="https://www.curseforge.com/members/ix0rai">curseforge</a></li>
+                    <li><a href="https://github.com/ix0rai">github</a></li>
+                    <li><a href="https://steamcommunity.com/id/ix0rai/">steam</a></li>
+                    <li><a href="https://www.twitch.tv/ix0rai/">twitch</a></li>
+                    <li><a href="https://youtube.com/@ix0rai">youtube</a></li>
+                    <li><a href="mailto:ix0rai64@gmail.com">email</a></li>
+                    <li><a href="https://en.pronouns.page/@ix0rai">pronouns.page</a></li>
+                    <li><a href="https://modrinth.com/user/ix0rai/">modrinth</a></li>
+                    <li><a href="https://twitter.com/ix0rai">twitter</a></li>
+                </ul>
             </div>
         </html>
     );
