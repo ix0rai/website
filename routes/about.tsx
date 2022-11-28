da1b693b910953889ddf3b00029d4fad77fb4672
ix0rai <ix0rai64@gmail.com>
2022-11-27 20:27:36 -0600

    try doing meta again

--- routes/about.tsx
+++ routes/about.tsx
@@ -3,8 +3,6 @@ import Wips from "../islands/Wips.tsx"
 import Links from "../islands/Links.tsx";
 
 export default function AboutPage() {
-    const date = new Date();
-    date.setHours(date.getHours() + 1);
     const projectList = <Projects filter=""/>;
 
     const wipList = <Wips filter=""/>;
@@ -12,47 +10,52 @@ export default function AboutPage() {
     const linkList = <Links filter=""/>;
 
     return (
-        <meta name="description" content="about ix0rai">
-            <title>ix0rai | about</title>
-            <div class="p-4 mx-auto max-w-screen-md" style="text-align:center">
-                <h1>info on me, ix0rai!</h1>
-                <br></br>
-                <p>my pronouns:</p>
-                <div class={"bg-purple-200 rounded p-2.5"}>
-                    <h2>she/her</h2>
+        <html>
+            <head>
+                <meta name="description" content="ix0rai - about me"/>
+            </head>
+            <body>
+                <title>ix0rai | about</title>
+                <div class="p-4 mx-auto max-w-screen-md" style="text-align:center">
+                    <h1>info on me, ix0rai!</h1>
+                    <br></br>
+                    <p>my pronouns:</p>
+                    <div class={"bg-purple-200 rounded p-2.5"}>
+                        <h2>she/her</h2>
+                    </div>
+
+                    <br></br>
+
+                    <p>I'm currently learning:</p>
+                    <ul class={"bg-blue-200 rounded p-2.5"}>
+                        <li>the rust programming language</li>
+                        <li>web development with fresh</li>
+                        <li>advanced minecraft modding</li>                
+                    </ul>
+
+                    <br></br>
+
+                    <p>my links:</p>
+                    <div class={"bg-pink-200 rounded p-2.5"}>
+                        {linkList}
+                    </div>
+
+                    <br></br>
+
+                    <p>my projects:</p>
+                    <div class={"bg-yellow-200 rounded p-2.5"}>
+                        {projectList}
+                    </div>
+
+                    <br></br>
+
+                    <p>my works in progress:</p>
+                    <div class={"bg-red-200 rounded p-2.5"}>
+                        {wipList}
+                    </div>
                 </div>
-
-                <br></br>
-
-                <p>I'm currently learning:</p>
-                <ul class={"bg-blue-200 rounded p-2.5"}>
-                    <li>the rust programming language</li>
-                    <li>web development with fresh</li>
-                    <li>advanced minecraft modding</li>                
-                </ul>
-
-                <br></br>
-
-                <p>my links:</p>
-                <div class={"bg-pink-200 rounded p-2.5"}>
-                    {linkList}
-                </div>
-
-                <br></br>
-
-                <p>my projects:</p>
-                <div class={"bg-yellow-200 rounded p-2.5"}>
-                    {projectList}
-                </div>
-
-                <br></br>
-
-                <p>my works in progress:</p>
-                <div class={"bg-red-200 rounded p-2.5"}>
-                    {wipList}
-                </div>
-            </div>
-        </meta>
+            </body>
+        </html>
     );
-  }
+}
   
